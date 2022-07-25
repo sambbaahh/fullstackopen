ef80a10b1596cb58c7f3c89d9125c88a8bd642d8
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-25 17:27:41 +0300

    exercises 2.1-2.5 done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,40 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Total = ({ sum }) => <b>Number of exercises {sum}</b>
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ part }) =>
+  <>
+    <Part
+      part={part}
+    />
+  </>
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.exercises,
+    0
+  )
+  
+
+  return (
+    <div>
+      <Header course={course.name} />
+
+      <>{course.parts.map(part =>
+        <Content key={part.id} part={part} />
+      )}
+      </>
+
+      <Total sum = {total}/>
+
+
+    </div>
+  )
+}
+
+export default Course
