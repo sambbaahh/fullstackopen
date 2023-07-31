3c5917a8207593055aec79c9a27b5774043274d6
SamiK <watchwiseapplication@gmail.com>
2023-07-31 21:07:26 +0300

    9.15 done

--- part9/my-app/src/App.tsx
+++ part9/my-app/src/App.tsx
@@ -1,67 +1,152 @@
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
-
-  interface HeaderProps {
-    name: string;
-  }
-
-  interface CoursePart {
+  interface CoursePartBase {
     name: string;
     exerciseCount: number;
   }
 
-  interface ContentProps {
-    courseParts: CoursePart[];
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+  interface CoursePartBasic extends CoursePartDescription {
+    kind: "basic"
   }
 
-  const Header = (props: HeaderProps) => {
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+
+  interface CoursePartBackground extends CoursePartDescription {
+    backgroundMaterial: string;
+    kind: "background"
+  }
+
+  interface CoursePartRequirement extends CoursePartDescription {
+    requirements: string[];
+    kind: "special"
+  }
+
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirement;
+
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
+    }
+  ];
+
+  const Part = ({ part }: { part: CoursePart }) => {
+    if (part.kind === "basic") {
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <p><i>{part.description}</i></p>
+        </div>
+      )
+    }
+    else if (part.kind === "group") {
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <p>project exercise {part.groupProjectCount}</p>
+        </div>
+      )
+    }
+    else if (part.kind === "background") {
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <p><i>{part.description}</i></p>
+          <p>submit to {part.backgroundMaterial}</p>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <p><i>{part.description}</i></p>
+          <p>required skills: {part.requirements.join(", ")}</p>
+        </div>
+      )
+    }
+  }
+
+  const Header = ({ name }: { name: string }): JSX.Element => {
+    return <h1>{name}</h1>;
+  };
+
+  interface Content {
+    name: string;
+    exerciseCount: number;
+  }
+
+  const Content = ({ parts }: { parts: CoursePart[] }) => {
     return (
-      <h1>{props.name}</h1>
+      <div>
+        {parts.map(part => {
+          return (
+            <div key={part.name}>
+              <Part part={part} />
+            </div>
+          )
+        }
+        )}
+      </div>
     )
   }
 
-  const Content = (props: ContentProps) => {
+  const Total = ({ content }: { content: Content[] }) => {
     return (
-      <div>
-        {props.courseParts.map((course: CoursePart, index: number) => (
-          <p key={index}>
-            {course.name} {course.exerciseCount}
-          </p>
-        ))}
-      </div>
-    );
-  };
-
-  const Total = (props:ContentProps) => {
-    return(
-      <p>
-      Number of exercises{" "}
-      {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-    </p>
+      <h3>
+        Number of exercises{" "}
+        {content.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </h3>
     )
   }
 
   return (
     <div>
       <Header name={courseName} />
-      <Content courseParts={courseParts} />
-      <Total courseParts={courseParts}/>
+      <Content parts={courseParts} />
+      <Total content={courseParts} />
     </div>
   );
 };
-
+//
 export default App;
